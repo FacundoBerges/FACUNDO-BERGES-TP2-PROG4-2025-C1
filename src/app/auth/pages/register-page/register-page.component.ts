fbcb8f2906c908d854859cb6efaad437b566c64b
Facundo Berges <facundo.h.berges@gmail.com>
2025-05-31 10:31:35 -0300

    add: fuente, tailwindcss y prettier config

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sn-register-page',
+  imports: [],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css'
+})
+export class RegisterPageComponent {
+
+}
