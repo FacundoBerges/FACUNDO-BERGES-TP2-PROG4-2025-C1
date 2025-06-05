903b229ca99910c576dd57fd8d5be5c2e1e84e8d
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-05 10:41:17 -0300

    add: configuración de modulos mongoose y config en appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostsModule } from './modules/posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthModule } from './modules/auth/auth.module';
+import { PostsModule } from './modules/posts/posts.module';
 import { UsersModule } from './modules/users/users.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
-  imports: [PostsModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ cache: true, envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    AuthModule,
+    PostsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
