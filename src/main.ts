997e7799712220430a747c884a14d032f97f08ec
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-05 10:39:28 -0300

    add: añadido pipe de validación global configurado

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const validationPipe: ValidationPipe = new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  });
+
+  app.useGlobalPipes(validationPipe);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
