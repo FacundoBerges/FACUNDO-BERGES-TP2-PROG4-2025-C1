1d0f5c44ce824804e4aeedbc471ba42b1053612b
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-18 11:53:28 -0300

    fix: migración de campo isDeleted, cambio de nombre de atributos

--- backend/src/modules/posts/dto/create-post.dto.ts
+++ backend/src/modules/posts/dto/create-post.dto.ts
@@ -1,11 +1,5 @@
 import { Transform } from 'class-transformer';
-import {
-  IsBoolean,
-  IsOptional,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreatePostDto {
   @MaxLength(100, {
@@ -22,13 +16,9 @@ export class CreatePostDto {
   @MaxLength(500, {
     message: 'La descripción no puede tener más de 500 caracteres.',
   })
-  readonly content: string;
+  readonly description: string;
 
   @IsOptional()
   @IsString({ message: 'La URL de la imagen debe ser una cadena de texto.' })
-  contentImageUrl?: string;
-
-  @IsOptional()
-  @IsBoolean({ message: 'El campo isDeleted debe ser un booleano.' })
-  readonly isDeleted?: boolean;
+  imageUrl?: string;
 }
