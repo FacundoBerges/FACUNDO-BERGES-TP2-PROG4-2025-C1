9af33edd86fafb106b7bb93c1857bc98c38292ba
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-18 10:59:09 -0300

    fix: agregado logger y cambio en verificación de token

--- backend/src/guards/auth.guard.ts
+++ backend/src/guards/auth.guard.ts
@@ -2,38 +2,43 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
+  Logger,
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { Observable } from 'rxjs';
 
 import { JwtPayload } from 'src/modules/auth/interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private readonly logger: Logger = new Logger(AuthGuard.name);
+
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: Request = context.switchToHttp().getRequest();
     const token: string | undefined = this.retrieveBearerToken(request);
 
-    if (!token)
+    if (!token) {
+      this.logger.warn('No se encontró token de autorización en la solicitud.');
       throw new UnauthorizedException('No se encontró token de autorización');
+    }
 
-    const payload: JwtPayload = this.jwtService.verify(token, {
-      secret:
-        this.configService.get<string>('JWT_SECRET') || process.env.JWT_SECRET,
-    });
+    let payload: JwtPayload;
 
-    if (!payload)
+    try {
+      payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+    } catch (err) {
+      this.logger.warn('Error al verificar el token:', err);
       throw new UnauthorizedException('Token de autorización inválido');
+    }
 
     request['user'] = payload;
 
