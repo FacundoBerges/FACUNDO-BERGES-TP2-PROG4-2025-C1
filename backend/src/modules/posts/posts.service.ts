e04b2b22f2ee98e9e3301ab8a4ae3ea264b4ba4c
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-18 12:02:37 -0300

    fix: implementación de métodos (creación de post, lazyload módulo usuarios, excepciones)

--- backend/src/modules/posts/posts.service.ts
+++ backend/src/modules/posts/posts.service.ts
@@ -1,22 +1,25 @@
 import {
+  BadRequestException,
   Injectable,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
+import { LazyModuleLoader } from '@nestjs/core';
 import { InjectModel } from '@nestjs/mongoose';
-import mongoose, { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
+import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
 import { NoContentException } from 'src/exceptions/no-content-exception.exception';
+import { SortOptions, SortOrder } from './interfaces/sort-by.type';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostDocument, Post } from './schemas/post.schema';
-import { SortOptions, SortOrder } from './interfaces/sort-by.type';
-import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
 
 @Injectable()
 export class PostsService {
   constructor(
     @InjectModel(Post.name)
     private readonly postModel: Model<PostDocument>,
+    private readonly lazyModuleLoader: LazyModuleLoader,
   ) {}
 
   async create(
@@ -25,17 +28,16 @@ export class PostsService {
     image?: Express.Multer.File,
   ) {
     if (image && image.destination && image.filename)
-      createPostDto.contentImageUrl = `${image.destination}/${image.filename}`;
+      createPostDto.imageUrl = `${image.destination}/${image.filename}`;
 
-    const newPost: Post = {
+    const newPost = new this.postModel({
       title: createPostDto.title,
-      description: createPostDto.content,
-      imageUrl: createPostDto.contentImageUrl,
-      isDeleted: createPostDto.isDeleted || false,
-      author: new mongoose.Types.ObjectId(userData.sub),
-    };
+      description: createPostDto.description,
+      imageUrl: createPostDto.imageUrl,
+      author: new Types.ObjectId(userData.sub),
+    });
 
-    return await this.postModel.create(newPost);
+    return await newPost.save();
   }
 
   async findAll(
@@ -46,15 +48,21 @@ export class PostsService {
     authorId?: string,
   ) {
     const orderNumber = orderBy === 'asc' ? 1 : -1;
-
     const sortOptions = {};
     sortOptions[sortBy] = orderNumber;
+    const authorFilter = authorId
+      ? { author: new Types.ObjectId(authorId) }
+      : {};
 
-    const authorFilter = authorId ? { authorId: authorId } : {};
+    const { UsersModule } = await import('../users/users.module');
+    const { UsersService } = await import('../users/users.service');
+    const usersModule = await this.lazyModuleLoader.load(() => UsersModule);
+    const usersService = usersModule.get(UsersService);
+
+    if (authorId) await usersService.validateId(authorId);
 
     const posts = await this.postModel
-      .find({ isDeleted: false })
-      .where(authorFilter)
+      .find({ isDeleted: false, ...authorFilter })
       .sort(sortOptions)
       .skip(offset)
       .limit(limit)
@@ -80,8 +88,8 @@ export class PostsService {
     const post = await this.postModel.findById(id).exec();
 
     if (
-      post?.author.toString() === userData.sub ||
-      userData.profile === 'admin'
+      post?.author.toString() !== userData.sub.toString() &&
+      userData.profile !== 'admin'
     ) {
       throw new UnauthorizedException(
         'No tienes permiso para eliminar esta publicación.',
@@ -103,16 +111,22 @@ export class PostsService {
       .exec();
   }
 
-  async addComment(id: string, commentId: string) {
+  public async updateCommentsCount(id: string, increment: boolean = true) {
+    const updateValue = increment ? 1 : -1;
     await this.validateId(id);
 
-    return await this.postModel
-      .findByIdAndUpdate(id, { $addToSet: { comments: commentId } })
+    return this.postModel
+      .findByIdAndUpdate(
+        id,
+        { $inc: { commentsCount: updateValue } },
+        { new: true },
+      )
       .exec();
   }
 
   public async validateId(id: string) {
-    if (!id) throw new NotFoundException('ID de publicación no proporcionado.');
+    if (!id)
+      throw new BadRequestException('ID de publicación no proporcionado.');
 
     const postExists = await this.postModel.exists({
       _id: id,
