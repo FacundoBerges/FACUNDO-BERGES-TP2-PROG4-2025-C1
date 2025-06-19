6d62d8aa38e1c2b480d65a8b1965e9f250252961
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-19 20:10:17 -0300

    fix: cambio de implementación de excepción

--- backend/src/exceptions/no-content-exception.exception.ts
+++ backend/src/exceptions/no-content-exception.exception.ts
@@ -2,13 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class NoContentException extends HttpException {
   constructor(message: string = 'No content available') {
-    super(
-      {
-        statusCode: HttpStatus.NO_CONTENT,
-        message: 'No Content',
-        cause: message,
-      },
-      HttpStatus.NO_CONTENT,
-    );
+    super(message, HttpStatus.NO_CONTENT);
   }
 }
