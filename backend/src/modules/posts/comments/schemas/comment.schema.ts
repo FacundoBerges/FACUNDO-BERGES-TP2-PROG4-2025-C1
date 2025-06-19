3247ad848dc16189f81422ff0a6b252ef6a86846
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-19 15:45:45 -0300

    fix: cambio de import para Types.ObjectId

--- backend/src/modules/posts/comments/schemas/comment.schema.ts
+++ backend/src/modules/posts/comments/schemas/comment.schema.ts
@@ -1,21 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 export type CommentDocument = HydratedDocument<Comment>;
 
 @Schema({ timestamps: true })
 export class Comment {
-  @Prop({ type: mongoose.Schema.Types.ObjectId, auto: true })
-  _id: mongoose.Types.ObjectId;
+  _id: Types.ObjectId;
 
   @Prop({ required: true, trim: true, minlength: 1, maxlength: 500 })
   content: string;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
-  author: mongoose.Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  author: Types.ObjectId;
 
-  @Prop({ type: mongoose.Types.ObjectId, ref: 'Post', required: true })
-  post: mongoose.Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'Post', required: true })
+  post: Types.ObjectId;
 
   @Prop({ default: false })
   isDeleted: boolean;
