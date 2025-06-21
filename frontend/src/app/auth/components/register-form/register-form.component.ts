b4bab148d9437a4fc84b9d25081e4da823f48616
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-21 19:10:05 -0300

    add: método submit, uso de archivos para imagen de perfil, validaciones

--- frontend/src/app/auth/components/register-form/register-form.component.ts
+++ frontend/src/app/auth/components/register-form/register-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, output, signal } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
@@ -8,7 +8,6 @@ import {
 
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
-import { FileUploadModule, UploadEvent } from 'primeng/fileupload';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputGroupModule } from 'primeng/inputgroup';
@@ -16,15 +15,18 @@ import { InputTextModule } from 'primeng/inputtext';
 import { MessageModule } from 'primeng/message';
 import { TextareaModule } from 'primeng/textarea';
 
-import { passwordMatchValidator } from '../../validators/password-match-validator';
+import { alphaOnlyValidator } from '../../validators/alpha-only.validator';
+import { invalidPasswordValidator } from '../../validators/invalid-password.validator';
+import { invalidUsernameValidator } from '../../validators/invalid-username.validator';
+import { passwordMatchValidator } from '../../validators/password-match.validator';
 import { FormErrorService } from '../../services/form-error.service';
+import { UserRegistration } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'sn-register-form',
   imports: [
     ButtonModule,
     DatePickerModule,
-    FileUploadModule,
     FloatLabelModule,
     InputGroupAddonModule,
     InputGroupModule,
@@ -37,36 +39,74 @@ import { FormErrorService } from '../../services/form-error.service';
   styleUrl: './register-form.component.css',
 })
 export class RegisterFormComponent {
-  private FormBuilder = inject(FormBuilder);
-  public FormErrorService = inject(FormErrorService);
-  public registerForm = this.FormBuilder.group(
+  private formBuilder = inject(FormBuilder);
+  public formErrorService = inject(FormErrorService);
+  public readonly minDate = new Date('1900-01-01');
+  public readonly maxDate = new Date();
+  public imageFile: File | null = null;
+  public hasFileSelected = computed(() => this.imageFile !== null);
+  public passwordVisible = signal(false);
+  public confirmPasswordVisible = signal(false);
+  public registerEvent = output<UserRegistration>();
+  public registerForm = this.formBuilder.group(
     {
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      surname: ['', [Validators.required, Validators.minLength(2)]],
-      email: ['', [Validators.required, Validators.email]],
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(8),
-          Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/),
-        ],
-      ],
-      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
-      birthdate: [
-        '',
-        [Validators.required, Validators.pattern(/^\d{2}\/\d{2}\/\d{4}$/)],
-      ],
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50), alphaOnlyValidator]],
+      surname: ['', [ Validators.required, Validators.minLength(2), Validators.maxLength(50), alphaOnlyValidator]],
+      email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100), Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(3), invalidUsernameValidator]],
+      password: ['', [Validators.required, Validators.minLength(8), invalidPasswordValidator]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8), invalidPasswordValidator]],
+      birthdate: ['', [Validators.required]],
       description: ['', [Validators.maxLength(500)]],
-      profilePicture: [''],
     },
     {
       validators: [passwordMatchValidator],
     }
   );
-  public passwordVisible = signal(false);
-  public confirmPasswordVisible = signal(false);
+
+  public getErrorMessage(controlName: string): string | void {
+    return this.formErrorService.getErrorMessage(
+      controlName,
+      this.registerForm
+    );
+  }
+
+  public togglePasswordVisibility(): void {
+    this.passwordVisible.update((visible) => !visible);
+  }
+
+  public toggleConfirmPasswordVisibility(): void {
+    this.confirmPasswordVisible.update((visible) => !visible);
+  }
+
+  public onFileSelected(fileEvent: Event): void {
+    const input = fileEvent.target as HTMLInputElement;
+    const image = input.files && input.files.length > 0 ? input.files[0] : null;
+
+    this.imageFile = image;
+  }
+
+  public onSubmit() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    const newUser: UserRegistration = {
+      name: this.name?.value.trim(),
+      surname: this.surname?.value.trim(),
+      email: this.email?.value.trim(),
+      username: this.username?.value.trim(),
+      password: this.password?.value,
+      birthdate: this.birthdate?.value,
+      description: this.description?.value.trim(),
+      profilePicture: this.imageFile,
+    };
+
+    this.registerEvent.emit(newUser);
+  }
+
+  // * Getters for form controls
 
   public get name(): AbstractControl | null {
     return this.registerForm.get('name');
@@ -102,28 +142,5 @@ export class RegisterFormComponent {
 
   public get profilePicture(): AbstractControl | null {
     return this.registerForm.get('profilePicture');
-  }
-
-  public getErrorMessage(controlName: string): string | void {
-    return this.FormErrorService.getErrorMessage(
-      controlName,
-      this.registerForm
-    );
-  }
-
-  public togglePasswordVisibility() {
-    this.passwordVisible.update((visible) => !visible);
-  }
-
-  public toggleConfirmPasswordVisibility() {
-    this.confirmPasswordVisible.update((visible) => !visible);
-  }
-
-  public onUpload(uploadEvent: UploadEvent) {
-    console.log('Handle register submit.', this.registerForm.value);
-  }
-
-  public onSubmit() {
-    console.log('Handle register submit.', this.registerForm.value);
   }
 }
