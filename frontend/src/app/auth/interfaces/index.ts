711d1ade2ead2716916dfc4e7fe4e7b9061793ba
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-21 22:14:12 -0300

    add: interfaz de respuesta jwt y archivo barril de interfaces en modulo auth

---
+++ frontend/src/app/auth/interfaces/index.ts
@@ -0,0 +1,3 @@
+export type { FooterLinkText } from './footer-link-text.interface';
+export type { JWToken } from './jwtoken.interface';
+export type { UserCredentials, UserRegistration } from './user.interface';
