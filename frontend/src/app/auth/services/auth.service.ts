db7db42039ac6240bddcc3b27f82b83c65bcfb32
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-21 22:24:37 -0300

    add: estructura base de authService, implementación de método para login y registro, guardado y carga de token jwt

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -1,29 +1,71 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { UserCredentials, UserRegistration, JWToken } from '../interfaces/';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {
-  login(username: string, password: string): boolean {
-    console.log(
-      `Attempting to log in with username: ${username} and password: ${password}`,
+export class AuthService implements OnInit {
+  private httpClient = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}/auth`;
+  private token: string | null = null;
+
+  ngOnInit(): void {
+    this.loadFromLocalStorage();
+  }
+
+  private loadFromLocalStorage(): void {
+    this.token = localStorage.getItem('token');
+  }
+
+  public saveToLocalStorage(token: string): void {
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
+
+  public login(userCredentials: UserCredentials): Observable<JWToken> {
+    return this.httpClient.post<JWToken>(
+      `${this.baseUrl}/login`,
+      userCredentials
     );
-
-    // Implement your login logic here
-    // For now, just return true to simulate a successful login
-    return true;
   }
 
-  logout(): void {
+  public register(user: UserRegistration): Observable<any> {
+    if (!user.profilePicture) {
+      console.warn('No profile picture provided, registering without it.');
+      console.info('Registering user with data:', user);
+      // return this.httpClient.post(`${this.baseUrl}/register`, user);
+      return of();
+    }
+    const formData = new FormData();
+
+    formData.append('profile-picture', user.profilePicture);
+    Object.entries(user).forEach(([key, value]) => {
+      if (value && value !== 'profile-picture') formData.append(key, value);
+    });
+
+    console.info('Registering user with data:', formData);
+
+    return of();
+    // return this.httpClient.post(`${this.baseUrl}/register`, formData);
+  }
+
+  public logout(): void {
     console.log('Logging out...');
-    // Implement your logout logic here
+
+    // TODO: Implement your logout logic here
+    localStorage.removeItem('token');
+    this.token = null;
   }
 
-  isAuthenticated(): boolean {
+  public isAuthenticated(): boolean {
     console.log('Checking authentication status...');
 
-    // Implement your authentication check logic here
-    // For now, just return true to simulate an authenticated user
+    // TODO: Implement authentication logic here
+    // !For now, just return true to simulate an authenticated user
     return true;
   }
 }
