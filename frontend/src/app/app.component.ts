c81fcc8eaf28568fbf0cdd00c1084f48281fa850
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-21 22:13:20 -0300

    add: servicio de mensajes de primeng

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'sn-root',
   imports: [RouterOutlet, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  providers: [MessageService],
 })
 export class AppComponent {}
