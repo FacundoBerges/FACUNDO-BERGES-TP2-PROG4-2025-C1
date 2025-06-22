f2f8504f13d700b1f6c412b3b0c0a63ded0091d9
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-22 20:53:38 -0300

    fix: cambio en nombrado de archivo (duplicidad de fecha), reestructura para return preventivo, agregado log para debugear

--- backend/src/modules/uploads/uploads.service.ts
+++ backend/src/modules/uploads/uploads.service.ts
@@ -1,21 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import * as path from 'path';
 
 @Injectable()
 export class UploadsService {
-  buildPublicFilePath(file: Express.Multer.File): string | undefined {
-    if (file && file.destination && file.filename) {
-      const destinationPathSegments = file.destination.split('public');
-      const destinationPath =
-        destinationPathSegments.length > 1
-          ? destinationPathSegments[1]
-          : file.destination;
-      const date = Date.now();
-      const filename = `${date}-${file.filename}`;
-      const filePath = path.join(destinationPath, filename);
-      return filePath.replace(/\\/g, '/');
-    }
+  private readonly logger = new Logger(UploadsService.name);
 
-    return undefined;
+  buildPublicFilePath(file: Express.Multer.File): string | undefined {
+    if (!file || !file.destination || !file.filename) return undefined;
+
+    this.logger.debug(
+      `Building public file path for file: ${file.filename}, 
+      destination: ${file.destination},
+      originalname: ${file.originalname},
+      mimetype: ${file.mimetype}
+      ---
+      file: 
+      ${JSON.stringify(file, null, 2)}`,
+    );
+
+    const destinationPathSegments = file.destination.split('public');
+    const destinationPath =
+      destinationPathSegments.length > 1
+        ? destinationPathSegments[1]
+        : file.destination;
+    const filePath = path.join(destinationPath, file.filename);
+
+    return filePath.replace(/\\/g, '/');
   }
 }
