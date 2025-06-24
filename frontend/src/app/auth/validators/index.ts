dd2153209f5cb420e2ca1c60db934e8223a801d0
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-24 17:42:43 -0300

    add: archivo barril para export de validaciones

---
+++ frontend/src/app/auth/validators/index.ts
@@ -0,0 +1,4 @@
+export { alphaOnlyValidator } from './alpha-only.validator';
+export { invalidPasswordValidator } from './invalid-password.validator';
+export { invalidUsernameValidator } from './invalid-username.validator';
+export { passwordMatchValidator } from './password-match.validator';
