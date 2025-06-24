21a257c9bafc0c77f1fa6f98dc508488f04e2f3a
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-24 17:40:26 -0300

    fix: cambio de cantidad de caracteres válidos en regex nombre de usuario

--- frontend/src/app/auth/validators/invalid-username.validator.ts
+++ frontend/src/app/auth/validators/invalid-username.validator.ts
@@ -1,6 +1,6 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-const USERNAME_REGEX = /^[a-zA-Z0-9][a-zA-Z0-9\-._]{3,99}$/;
+const USERNAME_REGEX = /^[a-zA-Z0-9][a-zA-Z0-9\-._]{2,29}$/;
 
 export const invalidUsernameValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const value: string = control.value?.trim();
