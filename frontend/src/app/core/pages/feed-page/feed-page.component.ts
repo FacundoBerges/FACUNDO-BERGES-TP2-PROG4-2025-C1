4dddb0690103e56dbff6b19cc4c5f5389c1a654f
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-26 12:03:30 -0300

    add: lista de publicaciones en pagina principal

--- frontend/src/app/core/pages/feed-page/feed-page.component.ts
+++ frontend/src/app/core/pages/feed-page/feed-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
+import { PostListComponent } from '../../components/post/post-list/post-list.component';
+
 @Component({
   selector: 'sn-feed-page',
-  imports: [],
+  imports: [PostListComponent],
   templateUrl: './feed-page.component.html',
-  styleUrl: './feed-page.component.css'
+  styleUrl: './feed-page.component.css',
 })
-export class FeedPageComponent {
-
-}
+export class FeedPageComponent {}
