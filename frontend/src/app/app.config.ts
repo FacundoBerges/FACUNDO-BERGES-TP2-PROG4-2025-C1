d5373a574e3f3ef0b9dbc5b97664f010746402a1
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-26 23:29:31 -0300

    add: interceptor para añadir Authorization header en solicitudes

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,11 @@ import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
@@ -10,12 +14,13 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { authTokenInterceptor } from './auth/interceptors/auth-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     provideExperimentalZonelessChangeDetection(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authTokenInterceptor])),
     provideRouter(routes),
     providePrimeNG({
       ripple: true,
---
+++ frontend/src/app/auth/interceptors/auth-token.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+import { AuthService } from '../services/auth.service';
+
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService: AuthService = inject(AuthService);
+  const token: string | null = authService.token;
+
+  if (token) {
+    const clonedReq = req.clone({
+      headers: req.headers.append('Authorization', `Bearer ${token}`),
+    });
+
+    return next(clonedReq);
+  }
+
+  return next(req);
+};
