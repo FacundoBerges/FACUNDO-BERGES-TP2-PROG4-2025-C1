9a2210234ac5668c364e767f48216fe9875575ac
Facundo Berges <facundo.h.berges@gmail.com>
2025-06-27 22:01:02 -0300

    add: redirección a pagina de posts al loguearse correctamente, transformación de link a signal de solo lectura

--- frontend/src/app/auth/pages/login-page/login-page.component.ts
+++ frontend/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 
 import { AuthService } from '../../services/auth.service';
-import { FooterLinkText, JWToken, UserCredentials } from '../../interfaces/';
+import { FooterLinkText, UserCredentials } from '../../interfaces/';
 import { FormBodyComponent } from '../../components/shared/form/form-body/form-body.component';
 import { FormComponent } from '../../components/shared/form/form.component';
 import { FormFooterComponent } from '../../components/shared/form/form-footer/form-footer.component';
@@ -32,27 +32,36 @@ export class LoginPageComponent implements OnDestroy {
   private $currentLogin: Subscription | null = null;
   private authService = inject(AuthService);
   private messageService = inject(MessageService);
+  private router = inject(Router);
+  private timeoutRef?: ReturnType<typeof setTimeout>;
   public footerLink = signal<FooterLinkText>({
     text: 'Regístrate',
     url: '/auth/register',
-  });
+  }).asReadonly();
 
   ngOnDestroy(): void {
     if (this.$currentLogin) {
       this.$currentLogin.unsubscribe();
       this.$currentLogin = null;
     }
+
+    if (this.timeoutRef) {
+      clearTimeout(this.timeoutRef);
+      this.timeoutRef = undefined;
+    }
   }
 
   public onLogin(credentials: UserCredentials): void {
     this.$currentLogin = this.authService.login(credentials).subscribe({
-      next: (resp: JWToken) => {
+      next: () => {
         this.messageService.add({
           severity: 'success',
           summary: 'Inicio de sesión exitoso',
           detail: 'Bienvenido de nuevo!',
         });
-        this.authService.saveToLocalStorage(resp.accessToken);
+        this.timeoutRef = setTimeout(() => {
+          this.router.navigate(['/home/feed']);
+        }, 1000);
       },
       error: (error: HttpErrorResponse) => {
         this.messageService.add({
