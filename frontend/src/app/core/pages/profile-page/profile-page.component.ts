4f03a33b3ac353e80cba4b934dfcad0228fe3aea
Facundo Berges <facundo.h.berges@gmail.com>
2025-07-03 10:10:20 -0300

    add: añadida funcionalidad  para carga de publicaciones, manejo de mensajes y errores

--- frontend/src/app/core/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/core/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,14 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
+import { MessageService } from 'primeng/api';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { DividerModule } from 'primeng/divider';
 
 import { User } from '@auth/interfaces/user.interface';
 import { AuthService } from '@auth/services/auth.service';
-import { Post } from '@core/interfaces/post';
+import { Post, Pagination, Sorting } from '@core/interfaces';
 import { PostService } from '@core/services/post.service';
 import { UserImageComponent } from '@core/components/profile/user-image/user-image.component';
 import { UserInfoComponent } from '@core/components/profile/user-info/user-info.component';
@@ -23,154 +26,96 @@ import { PostListComponent } from '@core/components/post/post-list/post-list.com
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css',
 })
-export class ProfilePageComponent implements OnInit {
-  private authService = inject(AuthService);
-  private postService = inject(PostService);
+export class ProfilePageComponent implements OnInit, OnDestroy {
+  private timeoutId?: ReturnType<typeof setTimeout>;
+  private router = inject(Router);
+  private readonly messageService = inject(MessageService);
+  private readonly authService = inject(AuthService);
+  private readonly postService = inject(PostService);
   public user = signal<User>({} as User);
   public posts = signal<Post[]>([]);
 
   ngOnInit(): void {
-    // Simulate fetching user data
-    const fetchedUser: User = {
-      _id: 'generated-id',
-      name: 'Nombre',
-      surname: 'Apellido',
-      email: 'email@mail.com',
-      username: 'nombreusuario',
-      birthday: new Date('1990-10-22'),
-      bio: 'Este es un ejemplo de biografía. Puedes personalizarlo como desees.',
-      profilePictureUrl: undefined,
-      isActive: true,
-      profile: 'user',
-      createdAt: new Date('2023-10-22T10:00:00Z'),
-    };
+    this.authService.loadUserProfile().subscribe({
+      next: (user: User) => {
+        this.user.set(user);
+        this.loadLatestPosts();
+      },
+      error: async (error: HttpErrorResponse) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail:
+            error.error.message ||
+            'Error al obtener los datos del usuario. Redirigiendo a la página de inicio.',
+        });
 
-    const fetchedPosts: Post[] = [
-      {
-        _id: '6860528592d405a5c4f26f4e',
-        title: 'Post de administrador',
-        description: 'Publicación de prueba para subir una imagen!',
-        imageUrl:
-          '/uploads/img/posts/1751143040088-4k-space-pictures-zp773pnlw9zp3jq7.jpg',
-        commentsCount: 0,
-        author: {
-          _id: '6854368fb4037ca91ff569af',
-          name: 'Admin',
-          surname: 'Sistema',
-          username: 'adminuser',
-          profilePictureUrl: null,
-        },
-        createdAt: '2025-06-28T20:37:25.520Z',
-        updatedAt: '2025-06-28T20:37:25.520Z',
-        likesCount: 0,
-        likes: [],
+        this.timeoutId = setTimeout(() => {
+          this.router.navigate(['/']);
+        }, 3000);
       },
-      {
-        _id: '685ff2744fe5ba0f4b82ad90',
-        title: 'Un nuevo post',
-        description: 'Testeando un nuevo posteo!',
-        imageUrl: null,
-        commentsCount: 0,
-        author: {
-          _id: '68542db8c2690b735bc0d443',
-          name: 'Pedro',
-          surname: 'Lopez',
-          username: 'pedrolopez123.-',
-          profilePictureUrl: '/uploads/img/users/1750636310018-esferas.jpeg',
-        },
-        createdAt: '2025-06-28T13:47:32.986Z',
-        updatedAt: '2025-06-28T13:47:32.986Z',
-        likesCount: 0,
-        likes: [],
-      },
-      {
-        _id: '685ff1e65ba7f5200fe1ff98',
-        title: 'Un título',
-        description: 'Una descripción de publicación!',
-        imageUrl: null,
-        commentsCount: 0,
-        author: {
-          _id: '68542db8c2690b735bc0d443',
-          name: 'Pedro',
-          surname: 'Lopez',
-          username: 'pedrolopez123.-',
-          profilePictureUrl: '/uploads/img/users/1750636310018-esferas.jpeg',
-        },
-        createdAt: '2025-06-28T13:45:10.106Z',
-        updatedAt: '2025-06-28T13:45:10.106Z',
-        likesCount: 0,
-        likes: [],
-      },
-      {
-        _id: '68545e6b425b665e0cd56392',
-        title: 'Mi segundo post',
-        description:
-          'Este es el contenido de mi segundo post en la red social, esta vez con imagen!',
-        imageUrl: '/uploads/img/posts/1750636310018-esferas.jpeg',
-        commentsCount: 3,
-        author: {
-          _id: '685429cc540ab43148ac9e55',
-          name: 'Juan',
-          surname: 'Pérez',
-          username: 'juanperez',
-          profilePictureUrl: null,
-        },
-        createdAt: '2025-06-19T19:00:59.473Z',
-        updatedAt: '2025-06-27T00:32:32.723Z',
-        likesCount: 2,
-        likes: [
-          {
-            _id: '685429cc540ab43148ac9e55',
-            name: 'Juan',
-            surname: 'Pérez',
-            username: 'juanperez',
-            profilePictureUrl: null,
-          },
-          {
-            _id: '68542db8c2690b735bc0d443',
-            name: 'Pedro',
-            surname: 'Lopez',
-            username: 'pedrolopez123.-',
-            profilePictureUrl: '/uploads/img/users/1750636310018-esferas.jpeg',
-          },
-        ],
-      },
-      {
-        _id: '68545d97425b665e0cd56390',
-        title: 'Mi primer post',
-        description: 'Este es el contenido de mi primer post en la red social',
-        imageUrl: null,
-        commentsCount: 0,
-        author: {
-          _id: '685429cc540ab43148ac9e55',
-          name: 'Juan',
-          surname: 'Pérez',
-          username: 'juanperez',
-          profilePictureUrl: null,
-        },
-        createdAt: '2025-06-19T18:57:27.081Z',
-        updatedAt: '2025-06-27T00:23:19.094Z',
-        likesCount: 2,
-        likes: [
-          {
-            _id: '685429cc540ab43148ac9e55',
-            name: 'Juan',
-            surname: 'Pérez',
-            username: 'juanperez',
-            profilePictureUrl: null,
-          },
-          {
-            _id: '68542db8c2690b735bc0d443',
-            name: 'Pedro',
-            surname: 'Lopez',
-            username: 'pedrolopez123.-',
-            profilePictureUrl: '/uploads/img/users/1750636310018-esferas.jpeg',
-          },
-        ],
-      },
-    ];
+    });
+  }
 
-    this.user.set(fetchedUser);
-    this.posts.set(fetchedPosts);
+  ngOnDestroy(): void {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+      this.timeoutId = undefined;
+    }
+  }
+
+  public onLike(post: Post): void {
+    const isLike = post.likes.some(
+      (likeAuthor) => likeAuthor._id === this.authService.currentUser?.sub
+    );
+
+    this.postService.likePost(post._id, isLike).subscribe({
+      next: (updatedPost: Post) => {
+        const updatedPosts = this.posts().map((p) =>
+          p._id === updatedPost._id ? updatedPost : p
+        );
+        this.posts.set(updatedPosts);
+
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Éxito',
+          detail: isLike
+            ? 'Has quitado tu like de la publicación.'
+            : 'Has dado like a la publicación.',
+        });
+      },
+      error: (error: HttpErrorResponse) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail:
+            error.error.message ||
+            'Error al dar like a la publicación. Inténtalo de nuevo más tarde.',
+        });
+      },
+    });
+  }
+
+  private loadLatestPosts() {
+    const defaultSorting: Sorting = { sortBy: 'createdAt', sortOrder: 'desc' };
+    const latestThreePosts: Pagination = { limit: 3, offset: 0 };
+    const authorId = this.user().sub;
+
+    this.postService
+      .getPosts(defaultSorting, latestThreePosts, authorId)
+      .subscribe({
+        next: (posts: Post[]) => {
+          this.posts.set(posts);
+        },
+        error: (error: HttpErrorResponse) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail:
+              error.error.message ||
+              'Error al cargar las publicaciones. Inténtalo de nuevo más tarde.',
+          });
+        },
+      });
   }
 }
