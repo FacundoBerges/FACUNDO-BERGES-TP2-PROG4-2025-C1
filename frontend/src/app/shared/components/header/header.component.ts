58ff9b08359141c3fe99efcb5cf5596e290dcbbc
Facundo Berges <facundo.h.berges@gmail.com>
2025-07-10 15:26:38 -0300

    fix: cambios en visualización de menú superior y direccionamiento a rutas de administrador

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 
 import { MegaMenuItem } from 'primeng/api';
 import { AvatarModule } from 'primeng/avatar';
@@ -14,16 +14,61 @@ import { AuthService } from '@auth/services/auth.service';
 })
 export class HeaderComponent {
   public readonly authService = inject(AuthService);
-  public items: MegaMenuItem[] = [
-    {
-      label: 'Inicio',
-      icon: 'pi pi-fw pi-home',
-      routerLink: '/home/feed',
-    },
-    {
-      label: 'Perfil',
-      icon: 'pi pi-fw pi-user',
-      routerLink: '/home/profile',
-    },
-  ];
+  public readonly items = computed<MegaMenuItem[]>(() => {
+    const items: MegaMenuItem[] = [
+      {
+        label: 'Inicio',
+        title: 'Ir al feed',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/home/feed',
+      },
+      {
+        label: 'Perfil',
+        title: 'Ir al perfil',
+        icon: 'pi pi-fw pi-user',
+        routerLink: '/home/profile',
+      },
+    ];
+
+    if (this.authService.currentUser?.profile === 'admin') {
+      items.push({
+        label: 'Administración',
+        icon: 'pi pi-fw pi-shield',
+        items: [
+          [
+            {
+              label: 'Usuarios',
+              items: [
+                {
+                  label: 'Gestionar usuarios',
+                  icon: 'pi pi-fw pi-users',
+                  routerLink: '/home/admin/users',
+                },
+              ],
+            },
+            {
+              label: 'Estadísticas',
+              items: [
+                {
+                  label: 'Ver estadísticas',
+                  icon: 'pi pi-fw pi-chart-bar',
+                  routerLink: '/home/admin/stats',
+                },
+              ],
+            },
+          ],
+        ],
+      });
+    }
+
+    if (this.authService.currentUser) {
+      items.push({
+        label: 'Cerrar sesión',
+        icon: 'pi pi-fw pi-sign-out',
+        command: () => this.authService.logout(),
+      });
+    }
+
+    return items;
+  });
 }
